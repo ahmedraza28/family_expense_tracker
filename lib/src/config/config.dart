b5939f0965d52c63dd82e3d383c7a0c0b1deb8f5
Abdur Rafay Saleem <arafaysaleem@gmail.com>
2022-06-26 04:24:19 +0500

    chore: remove base url dart key

--- lib/src/config/config.dart
+++ lib/src/config/config.dart
@@ -7,20 +7,6 @@ import 'package:flutter/foundation.dart';
 class Config {
   const Config._();
 
-  /// The base url of our REST API, to which all the requests will be sent.
-  /// It is supplied at the time of building the apk or running the app:
-  /// ```
-  /// flutter build apk --debug --dart-define=BASE_URL=www.some_url.com
-  /// ```
-  /// OR
-  /// ```
-  /// flutter run --dart-define=BASE_URL=www.some_url.com
-  /// ```
-  static const baseUrl = String.fromEnvironment(
-    'BASE_URL',
-    defaultValue: 'localhost:3000/api/v1',
-  );
-
   /// The client key for sentry SDK. The DSN tells the SDK where to
   /// send the events to.
   /// 
