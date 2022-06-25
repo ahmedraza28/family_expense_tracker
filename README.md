642cf9c997877ae04ecad1873b5c8341ea54fc27
Abdur Rafay Saleem <rafay.saleem@10pearls.com>
2022-06-26 04:10:22 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# family_expense_tracker
+
+A budgeting, expense tracking and bill reminding app that allows shared control of budgetbooks
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/src/config/routes/routes.dart
@@ -0,0 +1,102 @@
+// ignore_for_file: constant_identifier_names
+import 'package:flutter/material.dart';
+
+// Screens
+
+// Helpers
+import '../../helpers/typedefs.dart';
+
+/// A utility class that holds screen names for named navigation.
+/// This class has no constructor and all variables are `static`.
+@immutable
+class Routes {
+  const Routes._();
+
+  /// The route to be loaded when app launches
+  static const String initialRoute = AppStartupScreenRoute;
+
+  /// The route to be loaded in case of unrecognized route name
+  static const String fallbackRoute = NotFoundScreenRoute;
+
+  /// The name of the route for app startup screen
+  static const String AppStartupScreenRoute = '/app-startup-screen';
+
+  // The name for the auth widget screen
+  static const String AuthWidgetBuilderRoute = '/auth-widget-builder';
+
+  /// The name of the route for home dashboard screen
+  static const String HomeScreenRoute = '/home-screen';
+
+  /// The name of the route for unrecognized route screen
+  static const String NotFoundScreenRoute = '/route-not-found-screen';
+
+  /// The name of the route for login screen.
+  static const String LoginScreenRoute = '/login-screen';
+
+  /// The name of the route for qr scanner screen.
+  static const String QrScannerScreenRoute = '/qr-scanner-screen';
+
+  /// The name of the route for register screen.
+  static const String RegisterScreenRoute = '/register-screen';
+
+  /// The name of the route for forgot password screen.
+  static const String ForgotPasswordScreenRoute = '/forgot-password-screen';
+
+  /// The name of the route for change password screen.
+  static const String ChangePasswordScreenRoute = '/change-password-screen';
+
+  /// The name of the route for posts feed screen.
+  static const String PostsFeedScreenRoute = '/posts-feed-screen';
+
+  /// The name of the route for adding or editing a post screen.
+  static const String AddEditPostScreenRoute = '/add-edit-post-screen';
+
+  /// The name of the route for activities feed screen.
+  static const String ActivitiesFeedScreenRoute = '/activities-feed-screen';
+
+  /// The name of the route for adding or editing an activity screen.
+  static const String AddEditActivityScreenRoute = '/add-edit-activity-screen';
+
+  /// The name of the route for student finder screen.
+  static const String StudentFinderScreenRoute = '/student-finder-screen';
+
+  /// The name of the route for student profile screen.
+  static const String ProfileScreenRoute = '/profile-screen';
+
+  /// The name of the route for user preferences screen.
+  static const String UpdatePreferencesScreenRoute =
+      '/update-preferences-screen';
+
+  /// The name of the route for my sent/received friend requests screen.
+  static const String MyFriendRequestsScreenRoute =
+      '/my-friend-requests-screen';
+
+  /// The name of the route for timetable schedule generator screen.
+  static const String SchedulerScreenRoute = '/scheduler-screen';
+
+  /// The name of the route for my/students' timetables screen.
+  static const String TimetablesScreenRoute = '/timetables-screen';
+
+  /// The name of the route for timetable details screen.
+  static const String AddEditTeacherReviewScreenRoute =
+      '/add-edit-teacher-review-screen';
+
+  static final Map<String, RouteBuilder> _routesMap = {
+    NotFoundScreenRoute: (_) => const SizedBox.shrink(),
+    ForgotPasswordScreenRoute: (_) => const SizedBox.shrink(),
+    ChangePasswordScreenRoute: (_) => const SizedBox.shrink(),
+    AddEditActivityScreenRoute: (_) => const SizedBox.shrink(),
+    TimetablesScreenRoute: (_) => const SizedBox.shrink(),
+    AddEditTeacherReviewScreenRoute: (_) => const SizedBox.shrink(),
+  };
+
+  static RouteBuilder getRoute(String? routeName) {
+    return routeExists(routeName)
+        ? _routesMap[routeName]!
+        : _routesMap[Routes.NotFoundScreenRoute]!;
+  }
+
+  static bool routeExists(String? routeName) {
+    return _routesMap.containsKey(routeName);
+  }
+}
