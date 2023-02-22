e881cc8464dc74cd182618d70e7d75e1a68e2aaf
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2023-02-22 14:59:18 +0500

    add category mock

--- lib/src/features/categories/repositories/categories_repository.codegen.dart
+++ lib/src/features/categories/repositories/categories_repository.codegen.dart
@@ -3,6 +3,9 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 // Core
 import '../../../core/core.dart';
 
+// Enums
+import '../enums/category_type_enum.dart';
+
 // Models
 import '../models/category_model.codegen.dart';
 
@@ -12,8 +15,8 @@ part 'categories_repository.codegen.g.dart';
 @Riverpod(keepAlive: true)
 CategoriesRepository categoriesRepository(CategoriesRepositoryRef ref) {
   final firestoreService = ref.read(firestoreServiceProvider);
-  return CategoriesRepository(firestoreService);
-  // return MockCategoriesRepository();
+  // return CategoriesRepository(firestoreService);
+  return MockCategoriesRepository();
 }
 
 class CategoriesRepository {
@@ -48,4 +51,51 @@ class CategoriesRepository {
       merge: true,
     );
   }
+}
+
+class MockCategoriesRepository implements CategoriesRepository {
+  @override
+  Stream<List<CategoryModel>> getBookCategories({required int bookId}) {
+    return Stream.value(const [
+      CategoryModel(
+        id: 1,
+        name: 'Category 1',
+        imageUrl: 'https://picsum.photos/200/300',
+        categoryType: CategoryType.income,
+      ),
+      CategoryModel(
+        id: 2,
+        name: 'Category 2',
+        imageUrl: 'https://picsum.photos/200/300',
+        categoryType: CategoryType.expense,
+      ),
+      CategoryModel(
+        id: 3,
+        name: 'Category 3',
+        imageUrl: 'https://picsum.photos/200/300',
+        categoryType: CategoryType.expense,
+      ),
+      CategoryModel(
+        id: 4,
+        name: 'Category 4',
+        imageUrl: 'https://picsum.photos/200/300',
+        categoryType: CategoryType.income,
+      ),
+    ]);
+  }
+
+  @override
+  Future<void> addCategory({
+    required int bookId,
+    required CategoryModel category,
+  }) async {}
+
+  @override
+  Future<void> updateCategory({
+    required int bookId,
+    required CategoryModel category,
+  }) async {}
+  
+  @override
+  FirestoreService get _firestoreService => throw UnimplementedError();
 }
