fed82e2bd3e11eb2927963601a6c8ab98f4af8bb
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2023-03-23 15:41:46 +0500

    refact(BalanceTransfer): add export file

--- lib/src/features/balance_transfer/balance_transfer.dart
+++ lib/src/features/balance_transfer/balance_transfer.dart
@@ -1,3 +1,4 @@
 export 'models/balance_transfer_model.codegen.dart';
+export 'providers/balance_transfer_provider.codegen.dart';
 export 'screens/add_edit_balance_transfer_screen.dart';
 export 'widgets/balance_transfer_list_item.dart';
