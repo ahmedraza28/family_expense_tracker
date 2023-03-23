6ec2c2f6b7815b77d59fd976368c5f1205334682
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2023-03-23 17:11:24 +0500

    refact(Budgets): add routes

--- lib/src/config/routing/routes.dart
+++ lib/src/config/routing/routes.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import '../../features/auth/auth.dart';
 import '../../features/balance_transfer/balance_transfer.dart';
 import '../../features/books/books.dart';
+import '../../features/budgets/budgets.dart';
 import '../../features/calculator/calculator.dart';
 import '../../features/categories/categories.dart';
 import '../../features/home/home.dart';
@@ -30,13 +31,18 @@ class Routes {
   static const String fallbackRoute = NotFoundScreenRoute;
 
   // The name for the app startup screen
-  static const String BookConfigLoaderScreenRoute = '/book-config-loader-screen';
+  static const String BookConfigLoaderScreenRoute =
+      '/book-config-loader-screen';
 
   // The name for the auth widget screen
   static const String AuthWidgetBuilderRoute = '/auth-widget-builder';
 
   /// The name of the route for home dashboard screen
   static const String HomeScreenRoute = '/home-screen';
+
+  /// The name of the route for balance transfer screen
+  static const String AddEditBalanceTransferScreenRoute =
+      '/add-edit-balance-transfer-screen';
 
   /// The name of the route for unrecognized route screen
   static const String NotFoundScreenRoute = '/route-not-found-screen';
@@ -47,41 +53,24 @@ class Routes {
   /// The name of the route for books screen.
   static const String BooksScreenRoute = '/books-screen';
 
-  /// The name of the route for add/edit book screen.
-  static const String AddEditBookScreenRoute = '/add-edit-book-screen';
-
   /// The name of the route for categories screen.
   static const String CategoriesScreenRoute = '/categories-screen';
-
-  /// The name of the route for add/edit category screen.
-  static const String AddEditCategoryScreenRoute = '/add-edit-category-screen';
 
   /// The name of the route for wallets screen.
   static const String WalletsScreenRoute = '/wallets-screen';
 
-  /// The name of the route for add/edit wallet screen.
-  static const String AddEditWalletScreenRoute = '/add-edit-wallet-screen';
-
   /// The name of the route for calculator screen.
   static const String CalculatorScreenRoute = '/calculator-screen';
-
-  /// The name of the route for balance transfer screen.
-  static const String AddEditBalanceTransferScreenRoute = '/balance-transfer-screen';
 
   /// The name of the route for transactions screen.
   static const String TransactionsScreenRoute = '/transactions-screen';
 
-  /// The name of the route for add/edit transaction screen.
-  static const String AddEditTransactionScreenRoute = '/add-edit-transaction-screen';
-
   /// The name of the route for budgets screen.
   static const String BudgetsScreenRoute = '/budgets-screen';
 
-  /// The name of the route for add/edit budget screen.
-  static const String AddEditBudgetScreenRoute = '/add-edit-budget-screen';
-
   /// The name of the route for manage book access screen.
-  static const String ManageBookAccessScreenRoute = '/manage-book-access-screen';
+  static const String ManageBookAccessScreenRoute =
+      '/manage-book-access-screen';
 
   /// The name of the route for about screen.
   static const String AboutScreenRoute = '/about-screen';
@@ -99,17 +88,13 @@ class Routes {
     NotFoundScreenRoute: (_) => const NotFoundScreen(),
     HomeScreenRoute: (_) => const HomeScreen(),
     BooksScreenRoute: (_) => const BooksScreen(),
-    AddEditBookScreenRoute: (_) => const AddEditBookScreen(),
     CategoriesScreenRoute: (_) => const CategoriesScreen(),
-    AddEditCategoryScreenRoute: (_) => const AddEditCategoryScreen(),
     WalletsScreenRoute: (_) => const WalletsScreen(),
-    AddEditWalletScreenRoute: (_) => const AddEditWalletScreen(),
-    AddEditBalanceTransferScreenRoute: (_) => const AddEditBalanceTransferScreen(),
     CalculatorScreenRoute: (_) => const CalculatorScreen(),
     TransactionsScreenRoute: (_) => const TransactionsScreen(),
-    AddEditTransactionScreenRoute: (_) => const AddEditTransactionScreen(),
-    // BudgetsScreenRoute: (_) => const SizedBox.shrink(),
-    // AddEditBudgetScreenRoute: (_) => const SizedBox.shrink(),
+    BudgetsScreenRoute: (_) => const BudgetsScreen(),
+    AddEditBalanceTransferScreenRoute: (_) =>
+        const AddEditBalanceTransferScreen(),
     // ManageBookAccessScreenRoute: (_) => const SizedBox.shrink(),
     // AboutScreenRoute: (_) => const SizedBox.shrink(),
     // InsightsScreenRoute: (_) => const SizedBox.shrink(),
--- lib/src/features/budgets/budgets.dart
+++ lib/src/features/budgets/budgets.dart
@@ -1,8 +1,13 @@
+export 'models/budget_filters_model.dart';
 export 'models/budget_model.codegen.dart';
+export 'providers/budget_filters_providers.codegen.dart';
 export 'providers/budgets_provider.codegen.dart';
 export 'repositories/budgets_repository.codegen.dart';
 export 'screens/add_edit_budget_screen.dart';
 export 'screens/budgets_screens.dart';
 export 'widgets/add_budget_fab.dart';
+export 'widgets/budget_filters_bar.dart';
 export 'widgets/budget_list_item.dart';
 export 'widgets/budgets_list.dart';
+export 'widgets/filters/filters_bottom_sheet.dart';
+export 'widgets/filters/filters_list_view.dart';
